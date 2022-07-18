a45af8811cc749af2a14d20d15073de5b44d462d
ahsan Zaman <ahsan.zaman@xorbix.com>
2022-07-19 04:35:21 +0500

    First Commit

---
+++ MatrimonialApp/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using MatrimonialApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatrimonialApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ MatrimonialApp/Program.cs
@@ -0,0 +1,22 @@
+using MatrimonialApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
